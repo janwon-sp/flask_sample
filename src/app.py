946d0c80344c851c549f048b521aafd977867ec2
janwon_k <janwon.kim@sandpit.co.jp>
2020-12-14 10:41:22 +0900

    jsonレスポンスのサンプルを追加

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
@@ -22,6 +24,10 @@ def hello(name=None):
 def hello_query():
     name = request.args.get('name')
     return render_template('hello.html', title='Hello Query Flask', name=name)
+
+@app.route('/hello_json')
+def hello_json():
+    return jsonify({'greeting': 'こんにちわ！'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
